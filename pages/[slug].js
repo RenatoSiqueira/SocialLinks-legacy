396d0c76f7eb3b478b2447493575d43cfaf3c658
Renato Siqueira <renatoelysiqueira@gmail.com>
2020-10-13 09:10:16 -0300

    added new feats

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Prismic from "prismic-javascript";
+import { useRouter } from "next/router";
 
 const RedirectTo = () => {
-  return <h1>B</h1>;
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 2000);
+  }, []);
+  return <h1>Url Não Encontrada</h1>;
 };
+
+// export async function getServerSideProps({ params, res }) {
+//   // console.log(params);
+//   // const client = Prismic.client(`https://renatosiqueira.cdn.prismic.io/api/v2`);
+//   // const link = await client.getByUID("shortlink", params.slug);
+//   // console.log(link);
+//   // if (link) {
+//   //   res.statusCode = 301;
+//   //   res.setHeader("Location", link.data.destino.url);
+//   //   res.end();
+//   //   return;
+//   // }
+//   // return {
+//   //   props: {},
+//   // };
+// }
 
 export default RedirectTo;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import Prismic from "prismic-javascript";
 
-const Index = () => {
-  return <h1>A</h1>;
+import Component from "../components";
+
+const Index = ({ data }) => {
+  return <Component {...data} />;
 };
+
+export async function getServerSideProps(context) {
+  let subDomain = "renatosiqueira";
+
+  const fullUrl = context.req.headers.host;
+
+  if (fullUrl.indexOf("localhost" >= 0)) {
+    if (fullUrl.length > 14) {
+      subDomain = fullUrl.slice(0, fullUrl.indexOf("localhost") - 1);
+    }
+    const client = Prismic.client(`https://${subDomain}.cdn.prismic.io/api/v2`);
+    const box = await client.getSingle("box");
+    return {
+      props: {
+        data: box.data,
+      },
+    };
+  } else {
+    console.log("false");
+  }
+  return null;
+}
 
 export default Index;
